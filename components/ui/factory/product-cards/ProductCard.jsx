0933888ccb58a51ba2937bc0733931393cdd0575
xhifi <shifa.newversion@gmail.com>
2025-08-24 06:44:32 +0500

    fixed some design issues and added spinning text to the corner of product div

--- components/ui/factory/product-cards/ProductCard.jsx
+++ components/ui/factory/product-cards/ProductCard.jsx
@@ -1,26 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProductCardCartButton from "./ProductCardCartButton";
+import { SpinningText } from "../../spinning-text";
 
 const ProductCard = ({ product }) => {
   const discount = product?.discount > 0 && (product?.discount / 100) * product?.original_price;
   const discountedPrice = parseInt(discount && product?.original_price - discount);
-
+  console.log(product);
   return (
-    <div className="group border-background/25">
+    <div className="group border-background overflow-hidden border-1">
       <div className="w-full relative">
         <Image
           src={product.images[0].path}
           width={500}
           height={700}
           alt={`${product.name}-${product.id}`}
-          className="border-primary w-full aspect-[9/13] object-cover object-center"
+          className="border-background border-b w-full aspect-[9/13] object-cover object-center"
         />
-        <ProductCardCartButton product={product} />
+        {/* <ProductCardCartButton product={product} /> */}
         {product.discount > 0 && (
-          <span className="bg-destructive text-destructive-foreground rounded-full absolute top-3 right-3 flex items-center justify-center text-sm font-bold px-3 py-1">
-            OFF {product.discount}%
-          </span>
+          // <span className="bg-destructive text-destructive-foreground rounded-full absolute top-3 right-3 flex items-center justify-center text-sm font-bold px-3 py-1">
+          //   OFF {product.discount}%
+          // </span>
+          <div className="absolute top-12 right-12 inline-block text-red-600">
+            <SpinningText
+              radius={6}
+              fontSize={0.7}
+              className="font-light leading-none font-mono absolute top-1/2 left-1/2 -translate-x-1/5 -translate-y-1/2"
+              duration={15}
+            >
+              {`DISCOUNTED ★ DISCOUNTED ★ `}
+            </SpinningText>
+            <span className="text-2xl font-medium leading-none font-mono absolute z-10 top-1/2 left-1/2 -translate-x-1/5 -translate-y-1/2 -ms-2.5">
+              {`${product.discount}%`}
+            </span>
+          </div>
         )}
       </div>
       <Link href={`/shop/women/products/${product.id}`} className="px-3 block py-2">
@@ -30,7 +43,8 @@ const ProductCard = ({ product }) => {
         </p>
         {product.discount > 0 ? (
           <p className="text-secondary/75">
-            <span className="line-through text-sm">Rs {product.original_price.toLocaleString()}</span> ₨{discountedPrice.toLocaleString()}
+            <span className="line-through text-sm text-red-600">Rs {product.original_price.toLocaleString()}</span> ₨
+            {product.finalPrice.toLocaleString()}
           </p>
         ) : (
           <p className="text-secondary/75">Rs {product.original_price.toLocaleString()}</p>
