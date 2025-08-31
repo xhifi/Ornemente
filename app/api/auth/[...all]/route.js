14bfa3326a6f89a37edc77cb590308fcfe088333
xhifi <shifa.newversion@gmail.com>
2025-08-31 15:50:28 +0500

    create:auth init for better-auth

--- app/api/auth/[...all]/route.js
+++ app/api/auth/[...all]/route.js
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+import { auth } from "@/lib/auth-config";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth.handler);
+export const { GET, POST } = toNextJsHandler(auth);
