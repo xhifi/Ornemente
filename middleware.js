31403f5fb7eaafd8253c3fd4c5a1193abc59b4c2
xhifi <shifa.newversion@gmail.com>
2025-08-31 15:40:24 +0500

    update:added matcher path for dashboard, added session check and redirection for auth

--- middleware.js
+++ middleware.js
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
+import { getServerSession } from "./lib/auth-actions";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
   console.log(`MIDDLEWARE INVOKED`);
-  return NextResponse.redirect(new URL("/home", request.url));
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+  // return NextResponse.redirect(new URL("/home", request.url));
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/dashboard"],
   runtime: "nodejs",
 };
