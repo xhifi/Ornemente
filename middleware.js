a72a4c53f81837f34fe1903a54a91e363f5032df
xhifi <shifa.newversion@gmail.com>
2025-09-12 06:22:35 +0500

    update:minor

--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ import { getServerSession } from "./lib/auth-actions";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-  console.log(`MIDDLEWARE INVOKED`);
   const session = await getServerSession();
   if (!session) {
     return NextResponse.redirect(new URL("/login", request.url));
